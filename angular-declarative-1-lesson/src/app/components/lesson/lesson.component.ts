4a76d1fe13594c5c79a034b67452b925b29bce11
Daniel Altamar <danielaltamarb@gmail.com>
2023-01-26 16:37:10 -0500

    feat: rxjs installed, finish example of declarative code

--- angular-declarative-1-lesson/src/app/components/lesson/lesson.component.ts
+++ angular-declarative-1-lesson/src/app/components/lesson/lesson.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ItemsService } from 'src/app/services/items.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-lesson',
@@ -6,7 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./lesson.component.css']
 })
 export class LessonComponent {
-constructor(){
+// this component is using the ItemsService to keep track of the number of items in stock
+// and the map operator from the rxjs library to determine whether items can be purchased based
+// on the number of items in stock.
+  
+itemsLeftInStock$ = this.itemsService.itemsInStock$;
 
-}
+canPurchase$ = this.itemsLeftInStock$.pipe(
+  map((itemsLeftInStock:number) => (itemsLeftInStock > 0 ? true : false))
+  );
+constructor(private itemsService:ItemsService){}
+  buyItem (){
+    //updating itemsLeftInStock is handled by the observable now
+  }
 }
--- angular-declarative-1-lesson/src/app/services/items.service.ts
+++ angular-declarative-1-lesson/src/app/services/items.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ItemsService {
-
+  // it is not good practice to use any, we use it for the sake of the lesson
+  itemsInStock$:any;
   constructor() { }
 }
