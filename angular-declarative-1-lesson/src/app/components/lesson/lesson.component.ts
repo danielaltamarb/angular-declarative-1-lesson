64b6385e4d7419125e8a2b2cddd357787446c832
Daniel Altamar <danielaltamarb@gmail.com>
2023-01-26 09:56:54 -0500

    feat: generated lesson component and itemsService

---
+++ angular-declarative-1-lesson/src/app/components/lesson/lesson.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson',
+  template: `'./lesson.component.html'`,
+  styleUrls: ['./lesson.component.css']
+})
+export class LessonComponent {
+constructor(){
+
+}
+}
